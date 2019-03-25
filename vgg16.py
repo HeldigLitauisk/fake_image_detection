d8b15e758e01b529a5d4544629208bd9d93e8fd1
Sandor Bara <barasndor@gmail.com>
2019-03-25 22:51:59 +0100

    - new feature extractor - new classifier (vgg_16)

--- vgg16.py
+++ vgg16.py
@@ -1,101 +1,65 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import cv2
-import tensorflow as tf
-import os
-from os.path import isfile, isdir, getsize
-from os import mkdir, makedirs, remove, listdir
 
 from keras.models import Sequential
-from keras.applications import ResNet50
-from keras.applications.resnet50 import preprocess_input
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Dense
+from keras.applications import VGG16
 from keras import optimizers
-from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Dense, Dropout, Flatten
 
-#Global constants
+weights_path = 'D:\CBS\Master Thesis\CODE\\fake_image_detection\\vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+conv_base = VGG16(include_top=False, input_shape=(150,150,3), weights=weights_path)
 
-NUM_CLASSES = 2
-CHANNELS = 3
-IMAGE_SIZE = 224
-RESNET_POOLING = 'avg'
-DENSE_LAYER_ACTIVATION = 'softmax'
-OBJECTIVE_FUNCTION = 'categorical_crossentropy'
-LOSS_METRICS = ['accuracy']
 
-# Early_stop_patience must be less than num_epochs
-NUM_EPOCHS = 10
-EARLY_STOP_PATIENCE = 3
+#Load bottleneck features
 
-# These steps value should be proper FACTOR of no.-of-images in train & valid folders respectively
-# Training images processed in each step would be no.-of-train-images / STEPS_PER_EPOCH_TRAINING
-STEPS_PER_EPOCH_TRAINING = 10
-STEPS_PER_EPOCH_VALIDATION = 10
+bottleneck_features = np.load('vgg16_bottleneck_features.npz')
+train_features = bottleneck_features['train_features']
+train_labels = bottleneck_features['train_labels']
+validation_features = bottleneck_features['validation_features']
+validation_labels = bottleneck_features['validation_labels']
+test_features = bottleneck_features['test_features']
+test_labels = bottleneck_features['test_labels']
 
-# These steps value should be proper FACTOR of no.-of-images in train & valid folders respectively
-# NOTE that these BATCH* are for Keras ImageDataGenerator batching to fill epoch step input
-BATCH_SIZE_TRAINING = 100
-BATCH_SIZE_VALIDATION = 100
+#Reshape arrays
 
-# Using 1 to easily manage mapping between test_generator & prediction for submission preparation
-BATCH_SIZE_TESTING = 1
+train_features=np.reshape(train_features, (1224, 4 * 4 * 512))
+validation_features=np.reshape(validation_features, (409, 4 * 4 * 512))
+test_features=np.reshape(test_features, (408, 4 * 4 * 512))
 
-resnet_weights_path = 'D:\CBS\Master Thesis\CODE\\fake_image_detection\\resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+#Create classifier
 
 model = Sequential()
-model.add(ResNet50(include_top=False, pooling=RESNET_POOLING, weights=resnet_weights_path))
-model.add(Dense(NUM_CLASSES, activation=DENSE_LAYER_ACTIVATION))
-#First layer of ResNet50 not trainable, taking the original weights
-model.layers[0].trainable = False
-model.summary()
+model.add(Dense(256, activation='relu', input_dim= 4 * 4 * 512))
+model.add(Dropout(0.3))
+model.add(Dense(1, activation='sigmoid'))
 
-#Compile model
-sgd = optimizers.sgd(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(optimizer=sgd, loss=OBJECTIVE_FUNCTION, metrics=LOSS_METRICS)
+#adam = optimizers.adam(lr=0.01)
+sgd = optimizers.sgd(lr=2e-5, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
 
-#Process image, preprocess_input means batch normalization
-data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+history = model.fit(train_features, train_labels,
+                    epochs=300,
+                    batch_size=20,
+                    validation_data=(validation_features, validation_labels))
 
-train_generator = data_generator.flow_from_directory("D:\CBS\Master Thesis\CODE\Data_set real_and_fake_face\TRANSFER LEARNING\\training",
-                                                     target_size=(IMAGE_SIZE,IMAGE_SIZE), batch_size=BATCH_SIZE_TRAINING, class_mode='categorical')
-validation_generator = data_generator.flow_from_directory("D:\CBS\Master Thesis\CODE\Data_set real_and_fake_face\TRANSFER LEARNING\\validation",
-                                                    target_size=(IMAGE_SIZE,IMAGE_SIZE), batch_size=BATCH_SIZE_TRAINING, class_mode='categorical')
+#Graphs
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-#Train model
-cb_early_stopper = EarlyStopping(monitor='val_loss', patience=EARLY_STOP_PATIENCE)
-cb_checkpointer = ModelCheckpoint(filepath= "D:\CBS\Master Thesis\CODE\\fake_image_detection\\bestmodel.hdf5", monitor='val_loss', save_best_only=True, mode='auto')
+epochs = range(len(acc))
 
-#Fitting model
-fit_history = model.fit_generator(
-    train_generator,
-    steps_per_epoch=STEPS_PER_EPOCH_TRAINING,
-    epochs=NUM_EPOCHS,
-    validation_data=validation_generator,
-    validation_steps=STEPS_PER_EPOCH_VALIDATION,
-    callbacks=[cb_checkpointer, cb_early_stopper]
-)
-model.load_weights("D:\CBS\Master Thesis\CODE\\fake_image_detection\\bestmodel.hdf5")
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
 
-#Show training charts
-plt.figure(1, figsize=(15, 8))
+plt.figure()
 
-plt.subplot(221)
-plt.plot(fit_history.history['acc'])
-plt.plot(fit_history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'valid'])
-
-plt.subplot(222)
-plt.plot(fit_history.history['loss'])
-plt.plot(fit_history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'valid'])
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
 
 plt.show()
-
