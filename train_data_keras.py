c2f9830b9de107ca3b5ab815f6777034c56a367f
Eimantas Urbutis <eub@isynet.net>
2019-03-25 23:32:41 +0100

    Add grouped evaluation statements, load data once, fix image plot

--- train_data_keras.py
+++ train_data_keras.py
@@ -26,13 +26,13 @@ sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
 # CHANGE HERE
 OPTIMIZER = ['adam', sgd][0]
-IMG_SIZE = [224, 299][0]
+IMG_SIZE = [224, 299, 600, 96][3]
 POOLING = ['avg', 'max', None][0]
-DROPOUT = [0.3, 0.4, 0.5][2]
+DROPOUT = [0.3, 0.4, 0.5][1]
 DENSE_LAYER_ACTIVATION = ['softmax', 'sigmoid'][0]
 OBJECTIVE_FUNCTION = ['binary_crossentropy', 'categorical_crossentropy'][0]
 LOSS_METRIC = ['accuracy']
-TRANSFER_LEARNING = [ResNet50, VGG19, InceptionV3, MobileNetV2][1]
+TRANSFER_LEARNING = [ResNet50, VGG19, InceptionV3, MobileNetV2][3]
 
 INPUT_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
 LR = 1e-3
@@ -50,7 +50,7 @@ def create_transfer_learning_model():
     model = Sequential()
     tf.keras.backend.clear_session()
     model.add(TRANSFER_LEARNING(
-        include_top=False, pooling=POOLING, classes=NUMBER_OF_CLASSES))
+        include_top=False, pooling=POOLING, input_shape=INPUT_SHAPE))
     model.layers[0].trainable = False
     model.add(Dense(2, activation=DENSE_LAYER_ACTIVATION))
     return model
@@ -65,7 +65,7 @@ def label_img(img):
 
 
 def create_train_data(train_dir, data_name='train'):
-    npy_data_data = './data/{}_data.npy'.format(data_name)
+    npy_data_data = './data/{}_{}_data.npy'.format(IMG_SIZE, data_name)
     if os.path.exists(npy_data_data):
         print('Loaded from disk: {}'.format(npy_data_data))
         return np.load(npy_data_data)
@@ -78,10 +78,62 @@ def create_train_data(train_dir, data_name='train'):
         image = image.reshape(
             (1, image.shape[0], image.shape[1], image.shape[2]))
         image = preprocess_input(image)
-        training_data.append([image, np.array(label)])
+        training_data.append([image, np.array(label), path])
     shuffle(training_data)
     np.save(npy_data_data, training_data)
     return training_data
+
+
+def plot_data(data, model):
+    fig = plt.figure()
+    for num, data in enumerate(data[:24]):
+        img_num = data[1]
+        truth = 'Real' if img_num[0] == 1 else 'Fake'
+
+        y = fig.add_subplot(12, 2, num + 1)
+        orig = cv2.imread(data[2])
+        data = data[0].reshape(IMG_SIZE, IMG_SIZE, 3)
+        model_out = model.predict(np.array([data, ]))
+
+        if np.argmax(model_out) == 1:
+            str_label = 'P:Fake vs T:{}'.format(truth)
+        else:
+            str_label = 'P:Real vs T:{}'.format(truth)
+
+        y.imshow(orig, cmap='gray')
+        plt.title(str_label)
+        y.axes.get_xaxis().set_visible(False)
+        y.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+
+def load_data(data_dir, data_type='train'):
+    train_data = create_train_data(data_dir, data_type)
+    train = np.array([i[0] for i in train_data]).reshape(
+        -1, INPUT_SHAPE[0], INPUT_SHAPE[1], INPUT_SHAPE[2])
+    test = np.array([i[1] for i in train_data])
+    return train, test
+
+
+def evaluate_group(data, model, group_name='all'):
+    grouped_train = []
+    grouped_test = []
+    for element in data:
+        path = element[2]
+        if group_name in path:
+            grouped_train.append(element[0])
+            grouped_test.append(element[1])
+        elif group_name == 'fake' and 'real' not in path:
+            grouped_train.append(element[0])
+            grouped_test.append(element[1])
+        elif group_name == 'all':
+            grouped_train.append(element[0])
+            grouped_test.append(element[1])
+    train = np.array(grouped_train).reshape(
+        -1, INPUT_SHAPE[0], INPUT_SHAPE[1], INPUT_SHAPE[2])
+    test = np.array(grouped_test)
+    scores = model.evaluate(train, test, verbose=1)
+    print("Accuracy %s: %.2f%%" % (group_name, scores[1] * 100))
 
 
 def main():
@@ -111,21 +163,9 @@ def main():
                                histogram_freq=0, write_graph=True,
                                write_images=True)
 
-    train_data = create_train_data(train_dir)
-    validation_data = create_train_data(validation_dir, 'validation')
-    # test_data = create_train_data(test_dir, 'test')
-
-    train_x = np.array([i[0] for i in train_data]).reshape(
-        -1, INPUT_SHAPE[0], INPUT_SHAPE[1], INPUT_SHAPE[2])
-    test_x = np.array([i[1] for i in train_data])
-
-    train_y = np.array([i[0] for i in validation_data]).reshape(
-        -1, INPUT_SHAPE[0], INPUT_SHAPE[1], INPUT_SHAPE[2])
-    test_y = np.array([i[1] for i in validation_data])
-
-    # train_z = np.array([i[0] for i in test_data]).reshape(
-    #     -1, INPUT_SHAPE[0], INPUT_SHAPE[1], INPUT_SHAPE[2])
-    # test_z = np.array([i[1] for i in test_data])
+    train_x, test_x = load_data(train_dir, 'train')
+    train_y, test_y = load_data(validation_dir, 'validation')
+    train_z, test_z = load_data(test_dir, 'test')
 
     model = create_transfer_learning_model()
     model.compile(
@@ -133,34 +173,19 @@ def main():
     print(model.summary())
     model.fit(train_x, test_x, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS,
               validation_data=(train_y, test_y), callbacks=[tb_call_back])
-    scores = model.evaluate(train_y, test_y, verbose=1)
-    print("Accuracy: %.2f%%" % (scores[1] * 100))
+
+    validation_data = create_train_data(validation_dir, 'validation')
+    evaluate_group(validation_data, model)
+    evaluate_group(validation_data, model, 'real')
+    evaluate_group(validation_data, model, 'fake')
+    evaluate_group(validation_data, model, 'easy')
+    evaluate_group(validation_data, model, 'mid')
+    evaluate_group(validation_data, model, 'hard')
 
     model.save("./models/{}_model.h5".format(MODEL_NAME))
     model.save_weights('./weights/{}_weights.h5'.format(MODEL_NAME))
 
-    fig = plt.figure()
-    for num, data in enumerate(validation_data[:24]):
-        img_num = data[1]
-        truth = 'Real' if img_num[0] == 1 else 'Fake'
-        img_data = data[0]
-
-        y = fig.add_subplot(6, 4, num + 1)
-        # TODO: Fix this part!!!
-        orig = image.load_img(img_data)
-        data = img_data.reshape(IMG_SIZE, IMG_SIZE, 3)
-        model_out = model.predict(np.array([data, ]))
-
-        if np.argmax(model_out) == 1:
-            str_label = 'P:Fake vs T:{}'.format(truth)
-        else:
-            str_label = 'P:Real vs T:{}'.format(truth)
-
-        y.imshow(orig, cmap='gray')
-        plt.title(str_label)
-        y.axes.get_xaxis().set_visible(False)
-        y.axes.get_yaxis().set_visible(False)
-    plt.show()
+    plot_data(validation_data, model)
 
 
 if __name__ == "__main__":
